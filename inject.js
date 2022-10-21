f608938e5d94e7ac4e3645499041914c654496ce
sabri.artun <sabri.artun@mytheresa.com>
2022-10-21 16:12:33 +0200

    added object logics

--- inject.js
+++ inject.js
@@ -1,12 +1,21 @@
+var sweetAlert = document.createElement('script')
+sweetAlert.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js')
+document.head.appendChild(sweetAlert)
+
+
+
 jsonData = {
-    "objName": "dataLayer",
-    "website":"https://www.mytheresa.com/en-de/men.html",
+    "objName": "anLib.base.tms.eventCache",
+    "website": "tailor-acceptance",
     "tasks": {
         "task1": [
             {
                 "exec": `
 
                 `,
+
+                "event_name": "page_view",
+
                 "fields": [
                     {
                         "name": "gender",
@@ -34,38 +43,46 @@ jsonData = {
     }
 }
 
-if (window[jsonData["objName"]]) {
-    if (jsonData["tasks"]) {
-        if (Object.keys(jsonData["tasks"]).length > 0) {
-            for (let i = 0; i < Object.keys(jsonData["tasks"]).length; i++) {
+if (window.location.href.includes(jsonData.website)) {
 
-                if (localStorage.getItem("index") != null) {
-                    if (localStorage.getItem("index") != i + 1) {
-                        eval(jsonData["tasks"]["task" + (i + 1)][0].exec)
-                        localStorage.setItem("index", i + 1)
-                    }
-                }else{
-                    localStorage.setItem("index", 0)
-                    window.location.reload()
+    window.onload = (event) => {
+        var splitObj = jsonData.objName.split('.')
+        splitObj.forEach((e, i) => {
+            i == 0 ? eObj = window[e] : eObj = eObj[e]
+        });
+
+        for (let i = 0; i < Object.keys(jsonData["tasks"]).length; i++) {
+
+            //SAVE LAST TESTED TASK NUMBER TO LOCALSTORAGE
+            if (localStorage.getItem("index") != null) {
+                if (localStorage.getItem("index") != i + 1) {
+                    eval(jsonData["tasks"]["task" + (i + 1)][0].exec)
+                    localStorage.setItem("index", i + 1)
                 }
+            } else {
+                localStorage.setItem("index", 0)
+                window.location.reload()
+            }
 
-                for (let index = 0; index < jsonData["tasks"]["task" + (i + 1)][0].fields.length; index++) {
-                    fieldName = jsonData["tasks"]["task" + (i + 1)][0].fields[index].name
-                    fieldValue = new RegExp(jsonData["tasks"]["task" + (i + 1)][0].fields[index].value, "i")
 
-                    if (window[jsonData["objName"]][fieldName]) {
-                        if (window[jsonData["objName"]][fieldName].match(fieldValue) != null) {
-                            fresult = true
-                            console.log(fresult)
-                        } else {
-                            fresult = false
-                            console.log(fresult)
-                        }
+            //START TESTING THE TASKS
+            for (let index = 0; index < jsonData["tasks"]["task" + (i + 1)][0].fields.length; index++) {
+                eventName = jsonData["tasks"]["task" + (i + 1)][0].event_name
+                fieldName = jsonData["tasks"]["task" + (i + 1)][0].fields[index].name
+                fieldValue = new RegExp(jsonData["tasks"]["task" + (i + 1)][0].fields[index].value, "i")
+
+                eObj = eObj.filter(e => e.event_name == eventName)
+
+                if (eObj.fieldName) {
+                    if (window[jsonData["objName"]][fieldName].match(fieldValue) != null) {
+                        swal("Yes!", "Test Passed", "success");
+                    } else {
+                        swal("No!", "Test Failed", "error");
                     }
+                } else {
+                    swal("No!", "The required field name was not found in the tracking object. ", "error");
                 }
             }
         }
-    }
+    };
 }
-
-
