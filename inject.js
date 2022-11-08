2ad159aa958946a6bf57256b033b4ca8c83b8547
sabri.artun <sabri.artun@mytheresa.com>
2022-11-09 00:06:14 +0100

    timer added

--- inject.js
+++ inject.js
@@ -3,12 +3,9 @@ jsonData = {
     "website": "tailor-acceptance",
     "tasks": {
         "task1": {
+            "url": "https://tailor-acceptance.mytheresa.com/de/en/women",
             "exec": `
-            if(window.location.href!='https://tailor-acceptance.mytheresa.com/de/en/men'){
-                window.location.href='https://tailor-acceptance.mytheresa.com/de/en/men'
-            }else{
-                isReady = ()=>{}
-            }
+
                 `,
 
             "event_name": "page_view",
@@ -29,11 +26,76 @@ jsonData = {
             ]
         },
         "task2": {
+            "url": "https://tailor-acceptance.mytheresa.com/de/en/women/clothing",
             "exec": `
 
                 `,
 
-            "event_name": "add_to_cart",
+            "event_name": "product_list_view",
+
+            "fields": [
+                {
+                    "name": "atom",
+                    "value": ".*P00.*"
+                },
+                {
+                    "name": "atom_prd",
+                    "value": ".*value.*"
+                },
+                {
+                    "name": "consent",
+                    "value": ".*value.*"
+                },
+                {
+                    "name": "myth_persistent_eh",
+                    "value": ".*value.*"
+                },
+                {
+                    "name": "noproblem",
+                    "value": ".*value.*"
+                }
+            ]
+        },
+        "task3": {
+            "url": "https://tailor-acceptance.mytheresa.com/de/en/women/loewe-p00685941-p00685941",
+            "exec": `
+
+                `,
+
+            "event_name": "product_details",
+
+            "fields": [
+                {
+                    "name": "ecommerce.detail.products.0.id",
+                    "value": ".*P00.*"
+                },
+                {
+                    "name": "atom_prd",
+                    "value": ".*value.*"
+                },
+                {
+                    "name": "consent",
+                    "value": ".*value.*"
+                },
+                {
+                    "name": "myth_persistent_eh",
+                    "value": ".*value.*"
+                },
+                {
+                    "name": "noproblem",
+                    "value": ".*value.*"
+                }
+            ]
+        },
+        "task4": {
+            "url": "https://tailor-acceptance.mytheresa.com/de/en/women/loewe-p00685941-p00685941",
+            "exec": `
+                document.querySelectorAll('.productbuttons>div')[3].click()
+                document.querySelector('.variantslist>div').click()
+                document.querySelector('.productbuttons>div').click()
+                `,
+
+            "event_name": "product_cart_add",
 
             "fields": [
                 {
@@ -72,71 +134,101 @@ function readData(dlv) {
     return dlv
 }
 
-async function runTest(taskid) { // main function to run tests
+function runTest(taskid) { // main function to run tests
+
     var taskid = taskid
-    eval(jsonData.tasks[taskid].exec)
-    var eventName = jsonData.tasks[taskid].event_name
+    var continueTest = false
 
-    eventObject = readData(jsonData.objName).filter(e => e.event == eventName)
-
-    await isReady()
-    
-
-    if (eventObject.length > 0) { // if there is matching event
-        taskStatus = JSON.parse(localStorage.getItem('dl_verifier')) || {}
-        taskStatus[taskid] = []
-        jsonData.tasks[taskid].fields.forEach((e, i) => { //loop in expected fields
-            var fieldName = e.name
-            var fieldValue = new RegExp(e.value, "i")
-
-            //create status object for every field
-            taskStatus[taskid][i] = {
-                expectedName: undefined,
-                expectedValue: undefined,
-                currentValue: undefined,
-                status: undefined
-            }
-
-            if (readData(fieldName)) { // if there is expected variable in the event
-                taskStatus[taskid][i].expectedName = fieldName
-                if (readData(fieldName).match(fieldValue)) { // if expected variable matching with expected value
-                    taskStatus[taskid][i].expectedValue = e.value
-                    taskStatus[taskid][i].currentValue = readData(fieldName)
-                    taskStatus[taskid][i].status = 'passed'
-                } else {
-                    taskStatus[taskid][i].expectedValue = e.value
-                    taskStatus[taskid][i].currentValue = readData(fieldName)
-                    taskStatus[taskid][i].status = 'failed'
-                }
-            } else { // if there is not expected variable in the event
-                taskStatus[taskid][i].expectedName = fieldName
-                taskStatus[taskid][i].status = 'failed'
-            }
-        }) // end of the loop in fields
-        localStorage.setItem('dl_verifier', JSON.stringify(taskStatus))
+    if (window.location.href != jsonData.tasks[taskid].url) {
+        window.location.href = jsonData.tasks[taskid].url
     } else {
-        // if there is no matching event
-        taskStatus = JSON.parse(localStorage.getItem('dl_verifier')) || {}
-        taskStatus[taskid] = 'no event'
-        localStorage.setItem('dl_verifier', JSON.stringify(taskStatus))
-        //setTimeout(runTest(taskid), 1500) // try 1500 ms later again
+        continueTest = true
     }
 
-    // pass to the next task if there is
-    var taskid = 'task' + (parseInt(taskid.split('task')[1]) + 1)
-    console.log(taskid)
-    if (jsonData.tasks[taskid]) { runTest(taskid) }
+    if (continueTest == true) {
 
-} // End of the runTest function
+        eval(jsonData.tasks[taskid].exec)
 
-if (window.location.href.includes(jsonData.website)) {
+        setTimeout(() => {
+            step2()
+        }, 3000);
+
+        function step2() {
+            var eventName = jsonData.tasks[taskid].event_name
+            eventObject = readData(jsonData.objName).filter(e => e.event == eventName)
+
+            if (eventObject.length > 0) { // if there is matching event
+                taskStatus = JSON.parse(localStorage.getItem('dl_verifier')) || {}
+                taskStatus[taskid] = []
+                jsonData.tasks[taskid].fields.forEach((e, i) => { //loop in expected fields
+                    var fieldName = e.name
+                    var fieldValue = new RegExp(e.value, "i")
+
+                    //create status object for every field
+                    taskStatus[taskid][i] = {
+                        expectedName: undefined,
+                        expectedValue: undefined,
+                        currentValue: undefined,
+                        status: undefined
+                    }
+
+                    if (String(readData(fieldName))) { // if there is expected variable in the event
+                        taskStatus[taskid][i].expectedName = fieldName
+
+                        if (String(readData(fieldName)).match(fieldValue)) { // if expected variable matching with expected value
+                            taskStatus[taskid][i].expectedValue = e.value
+                            taskStatus[taskid][i].currentValue = String(readData(fieldName))
+                            taskStatus[taskid][i].status = 'passed'
+                        } else {
+                            taskStatus[taskid][i].expectedValue = e.value
+                            taskStatus[taskid][i].currentValue = String(readData(fieldName))
+                            taskStatus[taskid][i].status = 'failed'
+                        }
+                    } else { // if there is not expected variable in the event
+                        taskStatus[taskid][i].expectedName = fieldName
+                        taskStatus[taskid][i].status = 'failed'
+                    }
+                }) // end of the loop in fields
+                localStorage.setItem('dl_verifier', JSON.stringify(taskStatus))
+            } else {
+                // if there is no matching event
+                taskStatus = JSON.parse(localStorage.getItem('dl_verifier')) || {}
+                taskStatus[taskid] = 'no event'
+                localStorage.setItem('dl_verifier', JSON.stringify(taskStatus))
+                //setTimeout(runTest(taskid), 1500) // try 1500 ms later again
+            }
+
+            // pass to the next task if there is
+            taskid = 'task' + (parseInt(taskid.split('task')[1]) + 1)
+
+            if (jsonData.tasks[taskid]) { runTest(taskid) }
+
+        } // End of the runTest function
+    }
+}
+
+function dlVerifier(status) {
+    if (status == 'active') {
+        localStorage.setItem('dl_verifier_status', 'active')
+        window.location.reload()
+    } else if (status == 'passive') {
+        localStorage.setItem('dl_verifier_status', 'passive')
+    }
+}
+
+if (window.location.href.includes(jsonData.website) && localStorage.getItem('dl_verifier_status') == 'active') {
     if (localStorage.getItem('dl_verifier')) {
         var lastCompletedTask = Object.keys(JSON.parse(localStorage.getItem('dl_verifier'))).length
         var nextTask = 'task' + (lastCompletedTask + 1)
+
         if (jsonData.tasks[nextTask]) {
             runTest(nextTask)
+        } else {
+            localStorage.setItem('dl_verifier_status', 'passive')
         }
+
     } else {
         runTest('task1')
     }
 }
+
