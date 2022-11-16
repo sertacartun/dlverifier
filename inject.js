4fe40423db0e40d88cf1346c88ca65d8ac613825
sabri.artun <sabri.artun@mytheresa.com>
2022-11-16 14:19:31 +0100

    function structure changed and long functions divided to different functions

--- inject.js
+++ inject.js
@@ -123,6 +123,16 @@ jsonData = {
     }
 }
 
+
+function dlVerifier(status) {
+    if (status == 'active') {
+        localStorage.setItem('dl_verifier_status', 'active')
+        window.location.reload()
+    } else if (status == 'passive') {
+        localStorage.setItem('dl_verifier_status', 'passive')
+    }
+}
+
 function readData(dlv) {
     var dlv = dlv.split('.')
     if (dlv.includes('window')) {
@@ -134,103 +144,76 @@ function readData(dlv) {
     return dlv
 }
 
-function runTest(taskid) { // main function to run tests
-
+function checkStatus(taskid) { // main function to run tests
     var taskid = taskid
     var continueTest = false
 
-    if (window.location.href != jsonData.tasks[taskid].url) {
-        window.location.href = jsonData.tasks[taskid].url
-    } else {
-        continueTest = true
-    }
-
+    if (window.location.href != jsonData.tasks[taskid].url) { window.location.href = jsonData.tasks[taskid].url } else continueTest = true
+    
     if (continueTest == true) {
-
         eval(jsonData.tasks[taskid].exec)
+        setTimeout(() => { runTests (taskid)}, 3000);
+    }
+}
 
-        setTimeout(() => {
-            step2()
-        }, 3000);
+function runTests (taskid) {
+    var eventName = jsonData.tasks[taskid].event_name
+    eventObject = readData(jsonData.objName).filter(e => e.event == eventName)
 
-        function step2() {
-            var eventName = jsonData.tasks[taskid].event_name
-            eventObject = readData(jsonData.objName).filter(e => e.event == eventName)
+    if (eventObject.length > 0) { // if there is matching event
+        taskStatus = JSON.parse(localStorage.getItem('dl_verifier')) || {}
+        taskStatus[taskid] = []
+        jsonData.tasks[taskid].fields.forEach((e, i) => { //loop in expected fields
+            var fieldName = e.name
+            var fieldValue = new RegExp(e.value, "i")
 
-            if (eventObject.length > 0) { // if there is matching event
-                taskStatus = JSON.parse(localStorage.getItem('dl_verifier')) || {}
-                taskStatus[taskid] = []
-                jsonData.tasks[taskid].fields.forEach((e, i) => { //loop in expected fields
-                    var fieldName = e.name
-                    var fieldValue = new RegExp(e.value, "i")
-
-                    //create status object for every field
-                    taskStatus[taskid][i] = {
-                        expectedName: undefined,
-                        expectedValue: undefined,
-                        currentValue: undefined,
-                        status: undefined
-                    }
-
-                    if (String(readData(fieldName))) { // if there is expected variable in the event
-                        taskStatus[taskid][i].expectedName = fieldName
-
-                        if (String(readData(fieldName)).match(fieldValue)) { // if expected variable matching with expected value
-                            taskStatus[taskid][i].expectedValue = e.value
-                            taskStatus[taskid][i].currentValue = String(readData(fieldName))
-                            taskStatus[taskid][i].status = 'passed'
-                        } else {
-                            taskStatus[taskid][i].expectedValue = e.value
-                            taskStatus[taskid][i].currentValue = String(readData(fieldName))
-                            taskStatus[taskid][i].status = 'failed'
-                        }
-                    } else { // if there is not expected variable in the event
-                        taskStatus[taskid][i].expectedName = fieldName
-                        taskStatus[taskid][i].status = 'failed'
-                    }
-                }) // end of the loop in fields
-                localStorage.setItem('dl_verifier', JSON.stringify(taskStatus))
-            } else {
-                // if there is no matching event
-                taskStatus = JSON.parse(localStorage.getItem('dl_verifier')) || {}
-                taskStatus[taskid] = 'no event'
-                localStorage.setItem('dl_verifier', JSON.stringify(taskStatus))
-                //setTimeout(runTest(taskid), 1500) // try 1500 ms later again
+            //create status object for every field
+            taskStatus[taskid][i] = {
+                expectedName: undefined,
+                expectedValue: undefined,
+                currentValue: undefined,
+                status: undefined
             }
 
-            // pass to the next task if there is
-            taskid = 'task' + (parseInt(taskid.split('task')[1]) + 1)
+            if (String(readData(fieldName))) { // if there is expected variable in the event
+                taskStatus[taskid][i].expectedName = fieldName
 
-            if (jsonData.tasks[taskid]) { runTest(taskid) }
-
-        } // End of the runTest function
+                if (String(readData(fieldName)).match(fieldValue)) { // if expected variable matching with expected value
+                    taskStatus[taskid][i].expectedValue = e.value
+                    taskStatus[taskid][i].currentValue = String(readData(fieldName))
+                    taskStatus[taskid][i].status = 'passed'
+                } else {
+                    taskStatus[taskid][i].expectedValue = e.value
+                    taskStatus[taskid][i].currentValue = String(readData(fieldName))
+                    taskStatus[taskid][i].status = 'failed'
+                }
+            } else { // if there is not expected variable in the event
+                taskStatus[taskid][i].expectedName = fieldName
+                taskStatus[taskid][i].status = 'failed'
+            }
+        }) // end of the loop in fields
+        localStorage.setItem('dl_verifier', JSON.stringify(taskStatus))
+    } else {
+        // if there is no matching event
+        taskStatus = JSON.parse(localStorage.getItem('dl_verifier')) || {}
+        taskStatus[taskid] = 'no event'
+        localStorage.setItem('dl_verifier', JSON.stringify(taskStatus))
+        //setTimeout(checkStatus(taskid), 1500) // try 1500 ms later again
     }
-}
 
-function dlVerifier(status) {
-    if (status == 'active') {
-        localStorage.setItem('dl_verifier_status', 'active')
-        window.location.reload()
-    } else if (status == 'passive') {
-        localStorage.setItem('dl_verifier_status', 'passive')
-    }
-}
+    // pass to the next task if there is
+    taskid = 'task' + (parseInt(taskid.split('task')[1]) + 1)
+
+    if (jsonData.tasks[taskid]) { checkStatus(taskid) }
+
+} // End of the checkStatus function
 
 window.onload = function () {
     if (window.location.href.includes(jsonData.website) && localStorage.getItem('dl_verifier_status') == 'active') {
         if (localStorage.getItem('dl_verifier')) {
             var lastCompletedTask = Object.keys(JSON.parse(localStorage.getItem('dl_verifier'))).length
             var nextTask = 'task' + (lastCompletedTask + 1)
-
-            if (jsonData.tasks[nextTask]) {
-                runTest(nextTask)
-            } else {
-                localStorage.setItem('dl_verifier_status', 'passive')
-            }
-
-        } else {
-            runTest('task1')
-        }
+            if (jsonData.tasks[nextTask]) { checkStatus(nextTask) } else localStorage.setItem('dl_verifier_status', 'passive')
+        } else checkStatus('task1')
     }
 }
-
